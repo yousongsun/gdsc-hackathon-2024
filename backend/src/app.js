e4f8162edd46fcf438b0372c4b3ff893e8310c8f
yousongsun <yousongsun@outlook.com>
2024-08-31 11:10:29 +1200

    chore: Add express backend files and configuration

---
+++ backend/src/app.js
@@ -0,0 +1,45 @@
+// Configure environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+// Set's our port to the PORT environment variable, or 3000 by default if the env is not configured.
+const PORT = process.env.PORT ?? 3000;
+
+// Creates the express server
+const app = express();
+
+/**
+ * Configure middleware (logging, CORS support, JSON parsing support,
+ * static files support, cookie parser)
+ *
+ * CORS is configured to allow cookies and these two origins from fetch() requests.
+ * Feel free to reconfigure if required, or add your own middleware.
+ */
+app.use(morgan("combined"));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: [`http://localhost:${PORT}`, process.env.FRONTEND_ORIGIN],
+        credentials: true
+    })
+);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.static("public"));
+
+// Import and use our application routes.
+import routes from "./routes/routes.js";
+app.use("/", routes);
+
+// Make sure our database is up and running
+// import { getDatabase } from "./data/database.js";
+// await getDatabase();
+
+// Start the server running.
+app.listen(PORT, () => {
+    console.log(`PGCIT Final Project server listening on port ${PORT}`);
+});
