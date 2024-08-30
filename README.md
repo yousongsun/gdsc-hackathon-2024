d40cc239fa01757358cc5fc6f0d568fcfcd86894
yousongsun <yousongsun@outlook.com>
2024-08-31 08:57:02 +1200

    chore: Add initial project files and structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# HackBar Team - GDSC Hackathon 2024 Repository
+
---
+++ mobile/android/app/src/main/kotlin/com/hackbar/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hackbar.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
