5b70ca149abf490796f80d63aca09aa83e229d8c
Nathan Crasto <nathancrasto6@gmail.com>
2024-08-31 14:27:51 +1200

    design: sidebar toggle white

--- frontend/src/components/WebSidebar.jsx
+++ frontend/src/components/WebSidebar.jsx
@@ -5,7 +5,7 @@ const WebSidebar = ({ sidebarVisible, setsidebarVisible }) => {
 
   return (
     <div className={`${sidebarVisible ? 'w-1/4' : 'w-1/12'} p-4 flex-col h-screen text-5xl items-center`}>
-        <button className='fixed pl-12 z-20' onClick={() => setsidebarVisible(!sidebarVisible)}>≡</button>
+        <button className='fixed pl-12 z-20 text-white' onClick={() => setsidebarVisible(!sidebarVisible)}>≡</button>
         <div className={`${sidebarVisible ? 'flex' : 'hidden'} p-8 pt-24 pb-12 h-full flex-col justify-between fixed`}>
             <div className='flex flex-col'>
                 <Link to="/" className="font-bold p-4 py-8 text-white hover:bg-[#d2a0f9] transition-colors duration-300 text-5xl px-4 rounded-3xl">
