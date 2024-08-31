bb969737020498af70e072c4b563b2c5babb3147
Nathan Crasto <nathancrasto6@gmail.com>
2024-08-31 14:17:51 +1200

    feat: made sidebar sticky

--- frontend/src/components/WebSidebar.jsx
+++ frontend/src/components/WebSidebar.jsx
@@ -5,8 +5,8 @@ const WebSidebar = ({ sidebarVisible, setsidebarVisible }) => {
 
   return (
     <div className={`${sidebarVisible ? 'w-1/4' : 'w-1/12'} p-4 flex-col h-screen text-5xl items-center`}>
-        <button className='fixed pl-12' onClick={() => setsidebarVisible(!sidebarVisible)}>≡</button>
-        <div className={`${sidebarVisible ? 'flex' : 'hidden'} p-8 py-12 h-full flex-col justify-between fixed`}>
+        <button className='fixed pl-12 z-20' onClick={() => setsidebarVisible(!sidebarVisible)}>≡</button>
+        <div className={`${sidebarVisible ? 'flex' : 'hidden'} p-8 pt-24 pb-12 h-full flex-col justify-between fixed`}>
             <div className='flex flex-col'>
                 <Link to="/" className="p-4 hover:bg-gray-700 text-5xl">
                     Home
