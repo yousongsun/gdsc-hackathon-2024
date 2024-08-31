139a16cba1af823866c01bcdae1bef99c64633fb
minnp081 <mmet081@aucklanduni.ac.nz>
2024-08-31 14:40:42 +1200

    design: calendar header

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import Icon from '../assets/icon.jpg'
+
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className='container p-4 mx-auto'>
+      <div className='flex items-center'>
+        <img className='w-16 h-16' src={Icon} alt="Icon" />
+        <div className='text-3xl text-white font-bold'>Calendar</div>
+      </div>
+    </div>
   )
 }
 
