7921bd77e864545baf28416f6ae5fb8aca4908d1
zhengmianmian <zmianmian8@gmail.com>
2024-08-31 12:00:04 +1200

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import './App.css'
-
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Settings from "./components/Settings";
+import Prescriptions from "./components/Prescriptions";
+import AddPrescription from "./components/AddPrescription";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/prescriptions" element={<Prescriptions />} />
+          <Route path="/add-rescription" element={<AddPrescription />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
