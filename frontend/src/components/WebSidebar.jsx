a1b52ca25046fb412d1daac43a09450261f730ad
minnp081 <mmet081@aucklanduni.ac.nz>
2024-08-31 14:16:30 +1200

    design: update sidebar

--- frontend/src/components/WebSidebar.jsx
+++ frontend/src/components/WebSidebar.jsx
@@ -8,16 +8,16 @@ const WebSidebar = ({ sidebarVisible, setsidebarVisible }) => {
         <button onClick={() => setsidebarVisible(!sidebarVisible)}>≡</button>
         <div className={`${sidebarVisible ? 'flex' : 'hidden'} p-8 py-12 flex-col h-screen justify-between`}>
             <div className='flex flex-col'>
-                <Link to="/" className="p-4 hover:bg-gray-700 text-5xl">
+                <Link to="/" className="p-4 hover:bg-[#d2a0f9] transition-colors duration-300 text-5xl py-2 px-4 rounded-xl">
                     Home
                 </Link>
-                <Link to="/profile" className="p-4 hover:bg-gray-700 text-5xl">
+                <Link to="/profile" className="p-4 hover:bg-[#d2a0f9] transition-colors duration-300 text-5xl py-2 px-4 rounded-xl">
                     Profile
                 </Link>
-                <Link to="/prescriptions" className="p-4 hover:bg-gray-700 text-5xl">
+                <Link to="/prescriptions" className="p-4 hover:bg-[#d2a0f9] transition-colors duration-300 text-5xl py-2 px-4 rounded-xl">
                     Prescriptions
                 </Link>
-                <Link to="/calendar" className="p-4 hover:bg-gray-700 text-5xl">
+                <Link to="/calendar" className="p-4 hover:bg-[#d2a0f9] transition-colors duration-300 text-5xl py-2 px-4 rounded-xl">
                     Calendar
                 </Link>
             </div>
