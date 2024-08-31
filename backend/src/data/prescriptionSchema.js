b3c0d5e0fb54b914ad3e2d5c82aa9efea1521fbd
zhengmianmian <zmianmian8@gmail.com>
2024-08-31 16:00:44 +1200

    prescriptions GET works

---
+++ backend/src/data/prescriptionSchema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const prescriptionSchema = new Schema({
+  prescriptionName: { type: String, required: true },
+  medicationName: { type: String, required: true },
+  medicationDetails: { type: String, required: true },
+  administrationInstructions: { type: String, required: true },
+  additionalDetails: { type: String, required: true }
+});
+
+export const Prescription = mongoose.model("Prescription", prescriptionSchema);
