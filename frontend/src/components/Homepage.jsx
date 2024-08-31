31cfe523c15b00a73c50dc9fd93a6959a100732c
zhengmianmian <zmianmian8@gmail.com>
2024-08-31 13:53:43 +1200

    homepage prescriptio n placeholder

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -4,12 +4,12 @@ import CatIcon from "../assets/icon.jpg";
 const Homepage = () => {
   return (
     <div className="h-dvh mt-8 mx-auto w-1/2 flex flex-col items-center">
-      <div className="h-12 w-4/5">
+      <div className="h-12 w-4/5 text-2xl">
         <img className="inline h-full" src={CatIcon} alt="cat" />
         <span>Good Morning, Google Developers </span>
       </div>
-      <div className="bg-white my-4 rounded-xl w-4/5 h-64">a</div>
-      <div className="bg-white my-4 rounded-xl w-4/5 h-64">b</div>
+      <div className="bg-white my-4 rounded-xl w-4/5 h-64">prescription1</div>
+      <div className="bg-white my-4 rounded-xl w-4/5 h-64">prescription1</div>
     </div>
   );
 };
