8e1dc8ee550c4f3c9ac2fb06bd1c61ceda10d41a
minnp081 <mmet081@aucklanduni.ac.nz>
2024-08-31 15:43:57 +1200

    design: save changes button

--- frontend/src/components/AddPrescription.jsx
+++ frontend/src/components/AddPrescription.jsx
@@ -15,6 +15,7 @@ const AddPrescription = () => {
           <h2 className='text-lg text-gray-300 font-bold mb-2'>Create a new prescription</h2>
           <div className='flex justify-between'>
             <h2 className='text-3xl font-bold mb-2'>Prescription Name</h2>
+            <button className='bg-primary hover:bg-purple-500 text-white font-bold py-1 px-4 rounded-3xl mt-4 '>Save changes</button>
           </div>
         </div>
 
