99b6057bb98c155a2824d76e1108fef194f1b01c
zhengmianmian <zmianmian8@gmail.com>
2024-09-01 12:41:23 +1200

    Update PatientDetail.jsx

--- frontend/src/components/PatientDetail.jsx
+++ frontend/src/components/PatientDetail.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Icon from "../assets/icon.jpg";
-import Patient from "./Patient";
+import HomePrspItem from "./HomePrspItem";
 const mockPatient = {
   _id: "1",
   lastSeen: "14/7/2024",
@@ -10,8 +10,83 @@ const mockPatient = {
   gender: "prefer not to respond",
   birthDate: "14/2/2000",
 };
-
-function PatientDetail(props) {
+const prsps = [
+  {
+    hours: 2,
+    minutes: 25,
+    prspName: "Amoxicillin",
+    amount: 400,
+    dosage: 2,
+    taken: 4,
+    total: 14,
+    form: "tablets",
+    unit: "mg",
+  },
+  {
+    hours: 2,
+    minutes: 25,
+    prspName: "Metformin",
+    amount: 500,
+    dosage: 4,
+    taken: 26,
+    total: 55,
+    form: "tablets",
+    unit: "mg",
+  },
+  {
+    hours: 3,
+    minutes: 5,
+    prspName: "Nivolumab",
+    amount: 100,
+    dosage: 10,
+    taken: 4,
+    total: 12,
+    form: "ml",
+    unit: "mg",
+  },
+  {
+    hours: 9,
+    minutes: 25,
+    prspName: "Pembrolizumab",
+    amount: 25,
+    dosage: 4,
+    taken: 10,
+    total: 14,
+    form: "ml",
+    unit: "mg",
+  },
+  {
+    hours: 12,
+    minutes: 25,
+    prspName: "Semaglutide",
+    amount: 3,
+    dosage: 1,
+    taken: 9,
+    total: 10,
+    form: "tablet",
+    unit: "mg",
+  },
+  {
+    hours: 12,
+    minutes: 25,
+    prspName: "Losartan",
+    amount: 50,
+    dosage: 3,
+    taken: 15,
+    total: 24,
+    form: "tablets",
+    unit: "mg",
+  },
+];
+function PatientDetail() {
+  const prescriptions = prsps.map((p, index) => (
+    <div
+      key={index}
+      className="bg-white/60 duration-200 hover:bg-white/90 my-4 p-6 rounded-3xl h-64"
+    >
+      <HomePrspItem prspInfo={p} />
+    </div>
+  ));
   return (
     <div className="container p-4 mx-auto">
       <div className="flex items-center">
@@ -35,6 +110,8 @@ function PatientDetail(props) {
             {mockPatient.birthDate}
           </h2>
         </div>
+        <br />
+        <div className="container p-4 mx-auto">{prescriptions}</div>
       </div>
     </div>
   );
